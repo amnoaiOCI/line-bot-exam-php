e0d219359d92949d11758b68d5d42401a4b89b37
amnoaiOCI <32835926+amnoaiOCI@users.noreply.github.com>
2019-01-22 11:21:32 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ZWHqnexkkPRaWHDsvDneF7O1Nd/EsnAH0yECTARq/4GMQbejYMfgy81TxudEK2p44aeLHKa0VWiDGyxH4ujGIOrBpe8mkQkXuOAVC+mrgg+V/tpmQfkkQm/d5NisgJu9NtqK9L/TjD4cS3bH1GgHEQdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
